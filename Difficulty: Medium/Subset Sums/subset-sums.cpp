ab3ceab3cc589923f7312f22bd8f42fe1f2f5c75
Akshit Jain <146845857+akshitjain1@users.noreply.github.com>
2025-07-03 09:47:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Subset Sums/subset-sums.cpp
@@ -0,0 +1,24 @@
+class Solution {
+  public:
+    void func(vector<int> arr, vector<int>& result , int index ,int currentsum ){
+        if(index == arr.size()){
+            result.push_back(currentsum);
+                return;
+        } 
+      
+        //include.
+        func(arr,result,index+1, currentsum+arr[index]);
+    
+        //Exclude.
+        func(arr,result,index+1,currentsum);
+        
+        
+    };
+    
+    vector<int> subsetSums(vector<int>& arr) {
+        // code here
+        vector <int> sum;
+        func(arr,sum,0,0);
+        return sum;
+    }
+};
